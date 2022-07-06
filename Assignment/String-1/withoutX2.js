3d3f2ad9b069e775a70f4a4298228c87c5ee5ead
Renish Surani <77911976+renishsurani@users.noreply.github.com>
2022-07-06 23:23:46 +0530

    string1_withoutX2

---
+++ Assignment/String-1/withoutX2.js
@@ -0,0 +1,14 @@
+function withoutX2(str){
+  if(str.length == 1 && str.charAt(0) == 'x')
+    return '';
+  
+  if(str.length>=2)
+    if(str.charAt(0)=='x' && str.charAt(1)=='x')
+      return str.substring(2);
+    if(str.charAt(0)=='x')
+      return str.substring(1);
+    if(str.charAt(1)=='x')
+      return str.charAt(0)+str.substring(2);
+  
+  return str;
+}
