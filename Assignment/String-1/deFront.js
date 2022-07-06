d53a692bc79dcdfa6ad1bb38c9e5c698889892f6
Renish Surani <77911976+renishsurani@users.noreply.github.com>
2022-07-06 22:32:25 +0530

    string1_str.charAt(1) != 'b'

---
+++ Assignment/String-1/deFront.js
@@ -0,0 +1,12 @@
+function deFront(str){
+  if(str.length ==1 && str.charAt(0)!='a')
+    return "";
+  if(str.length >=2)
+    if(str.charAt(0)!='a' && str.charAt(1) != 'b')
+      return str.substring(2);
+    else if(str.charAt(0)!='a')
+      return str.substring(1);
+    else if(str.charAt(1) != 'b')
+      return "a" + str.substring(2);
+   return str;
+}
